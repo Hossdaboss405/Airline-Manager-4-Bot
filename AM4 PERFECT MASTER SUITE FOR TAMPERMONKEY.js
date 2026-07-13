2b5b7a07831ffb0ad922206705a78a45aae1713e
Hossdaboss405 <Flickbrandon@gmail.com>
2026-07-12 20:30:39 -0500

    AM4 PERFECT MASTER SUITE FOR TAMPERMONKEY.js

--- AM4 PERFECT MASTER SUITE FOR TAMPERMONKEY.js
+++ AM4 PERFECT MASTER SUITE FOR TAMPERMONKEY.js
@@ -4,7 +4,8 @@
 // @version      1.0
 // @description  try to take over the world!
 // @author       HOSS
-// @match        your url
+// @match        *://*://*
+// @match        *://airlinemanager.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=airlinemanager.com
 // @grant        none
 // ==/UserScript==
